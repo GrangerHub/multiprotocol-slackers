fa10c74be30e464c3f92562f2a8e273648ad39bf
SlackerLinux85 <SlackerLinux85@55599770-6f59-11de-83c4-53937bf5378e>
2009-07-20 01:35:07 +0000

    Merge latiku7 175

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -1516,7 +1516,7 @@ void Cmd_CallVote_f( gentity_t *ent )
   trap_Argv( 1, arg1, sizeof( arg1 ) );
   trap_Argv( 2, arg2, sizeof( arg2 ) );
 
-  if( strchr( arg1, ';' ) || strchr( arg2, ';' ) )
+  if( strchr( arg1plus, ';' ) )
   {
     trap_SendServerCommand( ent-g_entities, "print \"Invalid vote string\n\"" );
     return;
@@ -2080,7 +2080,7 @@ void Cmd_CallTeamVote_f( gentity_t *ent )
   trap_Argv( 1, arg1, sizeof( arg1 ) );
   trap_Argv( 2, arg2, sizeof( arg2 ) );
 
-  if( strchr( arg1, ';' ) || strchr( arg2, ';' ) )
+  if( strchr( arg1plus, ';' ) )
   {
     trap_SendServerCommand( ent-g_entities, "print \"Invalid team vote string\n\"" );
     return;
